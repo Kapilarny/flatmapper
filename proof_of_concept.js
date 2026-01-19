7594c80474ea4684cacf6adac288be81db3a8497
kapilarny <kapilarnyyt@gmail.com>
2026-01-19 01:13:45 +0100

    lol

--- proof_of_concept.js
+++ proof_of_concept.js
@@ -11,9 +11,33 @@ const generate_random_array = () => {
 }
 
 // Other way to write it is:
-console.log(
+// console.log(
 (new Array(1)).fill(undefined) // bootstrap our first map
     .map(e => new Array(6).fill([]).fill(undefined, 1)) // Create the arr + 0-5 iteration points
     .map(e => e.map((v, i, a) => i === 0 ? v : (a[0].push(Math.floor(Math.random() * 5+1)) && 0))) // Fill with random numbers
     [0][0] // Extract the result
-)
+// )
+
+
+const a_function_with_a_loop = () => {
+    let running = true;
+    let i = 10;
+
+    while (running) {
+        console.log(i);
+        i--;
+    }
+
+    return 'done';
+}
+
+// Lets rewrite it
+console.log(
+(new Array(1)).fill(undefined) // bootstrap our first map
+    .map(e => new Array(3).fill(undefined).map((_, i) => i == 0 ? true : i == 1 ? 10 : 
+    (a, b) => 
+        new Array(3).fill(undefined).map((_, j) => j == 0 ? () => a == 0 : j == 1 ? () => console.log(b) : () => b--))
+    .map((e, idx, a) => idx != 2 ? e : e(a[0], a[1]) ) // Populate the loop structure
+    [2][1]()
+    // .map((e) => idx != 2 ? e : e[2](e[0], e[1])
+))
